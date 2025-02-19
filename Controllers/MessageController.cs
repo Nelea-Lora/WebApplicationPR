2a060fa75b6deccebd833cd5e403ae0f51c8c406
Sceastlivii <149630976+Nelea-Lora@users.noreply.github.com>
2025-02-19 17:57:55 +0200

    Create a project and controller to work with HTTP requests

---
+++ Controllers/MessageController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationPR.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+        [HttpGet("message")]
+        public IActionResult GetMessage()
+        {
+            return Ok("Привет! Это сообщение из контроллера.");
+        }
+    }
+}
